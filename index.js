716cb6c6fdad2d4c4edd3989543d81d73e2bde12
Hingaz <hhinga04@gmail.com>
2023-10-05 12:23:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const submitData = {
+  userName: "Humphrey Hinga",
+  emailAdress: "hhinga04@gmail.com"
+
+} 
+ const newElement = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    accept: "application/json",
+  },
+
+  body: JSON.stringify(submitData),
+};
+
+fetch("http://localhost:3000/users",newElement) 
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+
+  .catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  })
+  
+
+  
+
+
 // Add your code here
